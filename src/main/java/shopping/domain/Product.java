0952a685ca4a68020a009c646690e27a34968e5a
sang5c <skah321@gmail.com>
2024-08-10 19:11:46 +0900

    feat: 상품은 이름, 가격, 이미지, 설명을 가진다

---
+++ src/main/java/shopping/domain/Product.java
@@ -0,0 +1,58 @@
+package shopping.domain;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+
+    private static final int MIN_NAME_LENGTH = 2;
+    private static final int MAX_NAME_LENGTH = 50;
+    private static final int MIN_PRICE = 0;
+    private static final int MAX_DESCRIPTION_LENGTH = 100;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private String description;
+
+    protected Product() {
+    }
+
+    private Product(String name, BigDecimal price, String description) {
+        validate(name, price, description);
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public static Product create(String name, BigDecimal price) {
+        return new Product(name, price, null);
+    }
+
+    public static Product create(String name, BigDecimal price, String description) {
+        return new Product(name, price, description);
+    }
+
+    private void validate(String name, BigDecimal price, String description) {
+        if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("상품 이름은 2자 이상 50자 이하여야 합니다.");
+        }
+
+        if (price.compareTo(BigDecimal.ZERO) <= MIN_PRICE) {
+            throw new IllegalArgumentException("상품 가격은 0보다 커야 합니다.");
+        }
+
+        if (description != null && description.length() > MAX_DESCRIPTION_LENGTH) {
+            throw new IllegalArgumentException("상품 설명은 100자 이하여야 합니다.");
+        }
+    }
+}
---
+++ src/test/java/shopping/domain/ProductTest.java
@@ -0,0 +1,51 @@
+package shopping.domain;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class ProductTest {
+
+    @DisplayName("0원 미만 상품은 예외가 발생한다")
+    @Test
+    void create() {
+        assertThatThrownBy(() -> Product.create("상품", BigDecimal.valueOf(-1)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품 이름이 2자 미만 50자 초과이면 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"한", "일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십1" })
+    void create2(String name) {
+        assertThatThrownBy(() -> Product.create(name, BigDecimal.valueOf(1000)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품 설명이 100자 초과이면 예외가 발생한다")
+    @Test
+    void create3() {
+        assertThatThrownBy(() ->
+                Product.create(
+                        "상품",
+                        BigDecimal.valueOf(1000),
+                        "백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백한자백1"
+                ))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("설명이 포함되거나, 포함되지 않는 상품을 생성할 수 있다")
+    @Test
+    void create4() {
+        assertThatCode(() -> {
+            Product.create("상품", BigDecimal.valueOf(1000));
+            Product.create("상품", BigDecimal.valueOf(1000), "상품 설명");
+        }).doesNotThrowAnyException();
+    }
+}
